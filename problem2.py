069c52a3ad279f6583e6858f47621500146f5705
sukshatharao <55244003+sukshatharao@users.noreply.github.com>
2019-12-22 12:58:00 +0530

    Add files via upload

--- problem2.py
+++ problem2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[18]:
 
 
 import os
@@ -25,27 +25,37 @@ def find_files(suffix, path):
     files = []
     if len(path)==0 or len(suffix)==0:
         return None
-    for i in os.listdir(path):
-        temp1 = path +  '/' + i
-        #print("temp1 is", temp1)
-        if os.path.isfile(temp1) and temp1.endswith(".c"):
-            files.append(temp1)
-        elif os.path.isdir(temp1):
-            files.extend(find_files(suffix, temp1))
-    return files
+    try:
+        for i in os.listdir(path):
+            temp1 = os.path.join(path,i)
+            #print("temp1 is", temp1)
+            if os.path.isfile(temp1) and temp1.endswith(".c"):
+                files.append(temp1)
+            elif os.path.isdir(temp1):
+                files.extend(find_files(suffix, temp1))
+        return files
+    except:
+        print("ooops! not a directory")
 
-
+print("\n\t\t\t\tTest case 1")
 files_1 = find_files('.c', 'testdir')
 print(files_1)
 
+print("\n\t\t\t\tTest case 2")
 files_2 = find_files('.c', 'testdir/subdir3')
 print(files_2)
 
+print("\n\t\t\t\tTest case 3")
 files_3 = find_files('.c', '')   #returns None, since there is no path defined
 print(files_3)
 
+print("\n\t\t\t\tTest case 4")
 files_4 = find_files('', 'testdir')  #returns None, since there is no suffix defined
 print(files_4)
+
+print("\n\t\t\t\tTest case 5")
+files_5 = find_files('.c', 'fakedir')  
+print(files_5)
 
 
 # In[ ]:
